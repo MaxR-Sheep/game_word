05425024e05f226dc872d33ccb0d4fd3fb430391
Maxime Ronjat <maxime.ronjat@gmail.com>
2023-09-22 17:26:33 +0200

    ajout fonction pour commencer le jeu

--- js/script.js
+++ js/script.js
@@ -27,3 +27,42 @@ let words = [
 /*----------------JS avec fonction---------------------------*/
 
 const wordText = document.querySelector(".word");
+const hintText = document.querySelector(".hint span");
+const timeText = document.querySelector(".time b");
+const inputField = document.querySelector("input");
+const refreshBtn = document.querySelector(".refresh-word");
+const checkBtn = document.querySelector(".check-word");
+
+let correctWord, timer;
+
+const initTimer = (maxTime) => {
+  clearInterval(timer);
+  timer = setInterval(() => {
+    if (maxTime > 0) {
+      maxTime--;
+      return (timeText.innertext = maxTime);
+    }
+    alert("Temps écouler!${correctWord.toUpperCase()} était le bon mot");
+    initGame();
+  }, 1000);
+};
+
+const initGame = () => {
+  initTimer(45);
+  let randomObj = words[Math.floor(Math.random() * words.length)];
+  let wordArray = randomObj.word.split("");
+
+  for (let i = wordArray.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+  }
+
+  wordText.innerText = wordArray.join("");
+  hintText.innerText = randomObj.hint;
+  correctWord = randomObj.word.toLowerCase();
+  inputField.value = "";
+  inputField.setAttribute("maxlength", correctWord.length);
+};
+initGame();
+
+// btn validation
