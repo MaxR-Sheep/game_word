86b60e63058a5c6f7347e5b043645192f2098bb7
Maxime Ronjat <maxime.ronjat@gmail.com>
2023-09-22 17:03:32 +0200

    ajout liste de mots

---
+++ js/script.js
@@ -0,0 +1,29 @@
+/*----------------------MOTS-----------------------------------*/
+
+let words = [
+  { word: "skywalker", hint: "Nom trés célébre dans Star Wars" },
+  {
+    word: "playstation",
+    hint: "Nom d'une console de Jeux Video ",
+  },
+  {
+    word: "smartphone",
+    hint: "Outil utiliser plusieurs fois par jour",
+  },
+  {
+    word: "manga",
+    hint: "Bande dessinée japonnais",
+  },
+  {
+    word: "pikachu",
+    hint: "Célébre petit compagnon jaune",
+  },
+  {
+    word: "javascript",
+    hint: "Langage de programmation",
+  },
+];
+
+/*----------------JS avec fonction---------------------------*/
+
+const wordText = document.querySelector(".word");
